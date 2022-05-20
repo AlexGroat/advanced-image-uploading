20f7476ffe40402feb7f8dde52eacdc40079d23d
Alex Groat <alex.groat.95@gmail.com>
2022-05-20 15:49:57 +0930

    into the pond episode

--- resources/js/components/App.vue
+++ resources/js/components/App.vue
@@ -1,19 +1,33 @@
 <template>
-<div>
-    <h2 class="text-xl text-center">
-        {{ message }}
-    </h2>
-</div>
+  <div class="mt-4">
+    <file-pond
+      name="image"
+      ref="pond"
+      lable-idle="Click to choose image or drag here"
+      server="/upload"
+      @init="filepondInitialized"
+      accepted-file-types="image/*"
+    />
+  </div>
 </template>
 
 <script>
+import vueFilePond from "vue-filepond";
+import "filepond/dist/filepond.min.css";
+import FilePondPluginFileValidateType from "filepond-plugin-file-validate-type";
 
+const FilePond = vueFilePond(FilePondPluginFileValidateType);
 
 export default {
-    data() {
-        return {
-            message: 'Hello World'
-        }
-    }    
-}
+  components: {
+    FilePond,
+  },
+
+  methods: {
+    filepondInitialized() {
+      console.log("Filepond ready");
+      console.log("Filepond object:", this.$refs.pond);
+    },
+  },
+};
 </script>
